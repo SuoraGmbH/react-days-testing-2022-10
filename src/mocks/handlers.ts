0604a658b057d359b0cac9deccaa7285af0b4119
Hans-Christian Otto <c.otto@suora.com>
2022-10-20 16:09:37 +0200

    added request handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,2 +1,28 @@
 import { rest } from "msw";
-export const handlers = [];
+export const handlers = [
+  rest.get("http://localhost:4712/projects", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: "1",
+          name: "iJS Conference mocked",
+          description:
+            "From Angular and React to WebAssembly; from Progressive Web Apps to JAMstack",
+        },
+        {
+          id: "2",
+          name: "Javascript Days",
+          description:
+            "Die großen Trainingsevents für JavaScript, Angular, React und HTML & CSS",
+        },
+        {
+          id: "3",
+          name: "JAX",
+          description:
+            "Die Konferenzen für Java, Architektur- und Software-Innovation",
+        },
+      ])
+    );
+  }),
+];
