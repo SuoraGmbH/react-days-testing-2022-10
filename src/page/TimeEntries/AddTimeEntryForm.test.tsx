751581f3879cf06f089f3da58d7ceb7fe10ce4d0
Hans-Christian Otto <c.otto@suora.com>
2022-10-20 15:59:10 +0200

    add loading indicator test

--- src/page/TimeEntries/AddTimeEntryForm.test.tsx
+++ src/page/TimeEntries/AddTimeEntryForm.test.tsx
@@ -1,8 +1,15 @@
 import AddTimeEntryForm from "./AddTimeEntryForm";
 import { render } from "../../tests/render";
+import { screen } from "@testing-library/react";
 
 describe("<AddTimeEntryForm />", () => {
   test("smoke detector :fire:", () => {
     render(<AddTimeEntryForm />);
+  });
+
+  test("initially renders a loading indicator", () => {
+    render(<AddTimeEntryForm />);
+
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
   });
 });
